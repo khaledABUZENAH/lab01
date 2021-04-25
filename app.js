f0d52b687edf31a3909b766c8dcfeb77afeec41f
khaledABUZENAH <djkw365@gmail.com>
2021-04-25 14:39:58 +0300

    console edited

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let userName=prompt ('please enter your name');
-console.log(userName);
+console.log('name '+userName);
 
 let tvShow=prompt ('what is your favourite tv show?');
-console.log(tvShow);
+console.log('fav tv show '+tvShow);
 
 let age=prompt ('how old are you?');
-console.log(age);
+console.log('age ' +age);
 
 let favTeam=prompt ('which team do you support?');
-console.log(favTeam);
+console.log('fav team '+favTeam);
 
 alert ('welcome to may web page ' + userName + ' your fav tv show is ' + tvShow + ' and your age is ' + age + ' your fav team is ' + favTeam);
