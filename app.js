13ec8150b39a2145a144a25c48c2a04c044533f1
khaledABUZENAH <djkw365@gmail.com>
2021-04-25 14:16:35 +0300

    new repo

---
+++ app.js
@@ -0,0 +1,13 @@
+let userName=prompt ('please enter your name');
+console.log(userName);
+
+let tvShow=prompt ('what is your favourite tv show?');
+console.log(tvShow);
+
+let age=prompt ('how old are you?');
+console.log(age);
+
+let favTeam=prompt ('which team do you support?');
+console.log(favTeam);
+
+alert ('welcome to may web page ' + userName + ' your fav tv show is ' + tvShow + ' and your age is ' + age + ' your fav team is ' + favTeam);
